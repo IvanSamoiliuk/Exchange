75eec46e2b6f8d4a2552d26158c7c1866587cf4d
Ivan Samoiliuk <bessob10@gmail.com>
2024-08-30 03:21:07 -0700

    Created COLORS constant

---
+++ src/constants/colors.ts
@@ -0,0 +1,9 @@
+export const COLORS = {
+  background: '#282B30',
+  white: '#FFFFFF',
+  black: '#000000',
+  modal: 'rgba(0, 0, 0, 0.5)',
+  blue: '#007bff',
+  gray: '#ccc',
+  transparent: 'transparent',
+};
