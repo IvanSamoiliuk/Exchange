1e1f0d10ed3732bd82175ac7fa7d37a640d96e89
Ivan Samoiliuk <bessob10@gmail.com>
2024-08-30 03:20:42 -0700

    Updated App

--- App.tsx
+++ App.tsx
@@ -1,118 +1,87 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useEffect} from 'react';
+import moment from 'moment';
+import {Provider, useDispatch, useSelector} from 'react-redux';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  fetchCurrency,
+  setSelectedBaseCurrency,
+  fetchCurrencies,
+} from './src/store/slices/currencySlice';
+import {store, AppDispatch} from './src/store/store';
+import {RootState} from './src/store/store';
+import {StyleSheet, SafeAreaView, StatusBar} from 'react-native';
+import {CurrencyPicker} from './src/components/CurrencyPicker';
+import {CurrencyChart} from './src/components/CurrencyChart';
+import {CurrencyHistory} from './src/components/CurrencyHistory';
+import {COLORS} from './src/constants/colors';
+import {CurrencyData} from './src/models/types';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const dispatch = useDispatch<AppDispatch>();
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+  const selectedBaseCurrency = useSelector(
+    (state: RootState) => state.currency.selectedBaseCurrency,
   );
-}
+  const currencyData: CurrencyData[] = useSelector(
+    (state: RootState) => state.currency.data,
+  );
+  const currencies = useSelector(
+    (state: RootState) => state.currency.currencies,
+  );
+  const date = moment().format('DD.MM.YYYY');
+  const rate = currencyData.length > 0 ? parseFloat(currencyData[0].rate) : NaN;
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  useEffect(() => {
+    dispatch(fetchCurrencies());
+  }, [dispatch]);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  useEffect(() => {
+    if (selectedBaseCurrency) {
+      dispatch(
+        fetchCurrency({
+          base: selectedBaseCurrency,
+        }),
+      );
+    }
+  }, [selectedBaseCurrency, dispatch]);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor={COLORS.transparent} barStyle="dark-content" />
+      <CurrencyPicker
+        selectedCurrency={selectedBaseCurrency}
+        onCurrencyChange={currency =>
+          dispatch(setSelectedBaseCurrency(currency))
+        }
+        currencies={currencies}
+        date={date}
+        rate={rate}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <CurrencyChart
+        data={currencyData}
+        rate={rate}
+        currency={selectedBaseCurrency}
+      />
+      <CurrencyHistory baseCurrency={selectedBaseCurrency} />
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    backgroundColor: COLORS.white,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  historyContainer: {
+    marginTop: 20,
+    flex: 1,
   },
 });
 
-export default App;
+const RootApp = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+export default RootApp;
